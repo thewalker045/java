241193dcd8cd7eccb11efa2e7bd78d662d72a175
diwakar <2024cs_diwakarsharmaaditya_a@nie.ac.in>
2026-04-21 12:30:48 +0530

    newcommit

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,6 @@ void main() {
     for (int i = 1; i <= 5; i++) {//TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
         // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
         IO.println("i = " + i);
+        
     }
 }
